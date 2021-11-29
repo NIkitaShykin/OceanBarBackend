cf14f1c67c295ec95ea9b323702bcc8a78aa4ecf
Polina <polmi713@gmail.com>
2021-11-29 15:59:01 +0300

    add 'update order' request

--- src/services/tokens.service.ts
+++ src/services/tokens.service.ts
@@ -2,7 +2,7 @@ import * as JWT from 'jsonwebtoken'
 require('dotenv').config()
 
 function generateTokens(payload: {id: number}): {accessToken: string, refreshToken: string} {
-    const accessToken: string = JWT.sign(payload, process.env.JWT_SECRET, {expiresIn: '30m'})
+    const accessToken: string = JWT.sign(payload, process.env.JWT_SECRET, {expiresIn: '30s'})
     const refreshToken: string = JWT.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30d'})
     return {accessToken: accessToken, refreshToken: refreshToken}
 }
