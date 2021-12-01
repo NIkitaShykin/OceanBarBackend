599f4d1b4128e0599fd61109f52434743e415284
luchinayan <yanluchina48@gmail.com>
2021-12-01 19:18:25 +0300

    fix order route middleware

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -14,14 +14,13 @@ orderRouter.post('/', authMiddleware, OrderController.addOrder)
 // /api/order/ get orders for user
 orderRouter.get('/', authMiddleware, OrderController.getOrders)
 orderRouter.get('/orders', OrderController.getAllOrders)
-orderRouter.patch('/orders', OrderController.updateOrder)
 orderRouter.get('/takeaway', authMiddleware, OrderController.getTimeForTakeaway)
 orderRouter.get('/dishes/:order_id', authMiddleware, OrderController.getDishes)
 // /api/order/ get order by ID for user
 orderRouter.get('/:order_id', authMiddleware, OrderController.getOrderById)
 
 // /api/order/ update order state by ID for user
-orderRouter.patch('/:order_id', /*authMiddleware*/ OrderController.updateOrder)
+orderRouter.patch('/:order_id', authMiddleware, OrderController.updateOrder)
 
 /* // api/order/:order_id delete order from DB
 orderRouter.delete('/:order_id', authMiddleware, OrderController.deleteOrder)
